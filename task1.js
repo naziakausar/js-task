17708fda7d03aa92430e401e82835734d58aaf21
nazia kausar <kausarnazia07@gmail.com>
2024-01-16 10:34:28 +0500

    button model changes

--- task1.js
+++ task1.js
@@ -7,38 +7,28 @@
 //   }
 // }
 
-
-// function openpopup() {
-
-//   document.getElementById("popupval").style.display = "block";
- 
-// }
-// function hidepopup() {
-
-//   document.getElementById("popupval").style.display = "none";
- 
-// }
-
-
-function handlePopup() {
+// var form =document.getElementById('popupVal')
+//   function handlePopup(){form.style.display ="block";}
+//   var form =document.getElementById('popupVal')
+//  function closePopup(){form.style.display ="none";}
+ function handlePopup() {
   var form = document.getElementById("popupVal");
-   if (form.style.display === "none") {
-    form.style.display = "block";
-   } else {
+    if (form.style.display === "none") {
+     form.style.display = "block";
+    } else {
     form.style.display = "none";
     
-  }
+   }
   
-}
-function showResult(){
-  var form =
-  document.getElementsByClassName("popupVal");
-  if (form.style.display === "block") {
-    form.style.display = "none";
-   } else {
-    form.style.display = "block";
+ }
+// function showResult(){
+//   var form =
+//   document.getElementsByClassName("popupVal");
+//  if (form.style.display === "block") {    form.style.display = "none";
+//    } else {
+//     form.style.display = "block";
     
-  }
+// }
   
-  
-}
+// } 
+
