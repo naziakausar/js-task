e9e49b865a3f7d5d9682124ca18aa1b95a1f8861
Nazia Kausar <kausarnazia07@gmail.com>
2024-02-03 13:06:02 +0500

    first commit

--- task8.js
+++
@@ -1,5 +0,0 @@
-function random(){
-    document.getElementById("Number").innerHTML = Math.floor(Math.random()*10);
-}
-let name = "nazia";
-console.log("this is string" + name);
--- task9.js
+++
@@ -1,6 +0,0 @@
-function mouseOver() {
-    document.getElementById("demo").style.backgroundColor ="green";
-}
-function mouseOut() {
-    document.getElementById("demo").style.backgroundColor ="red";
-}
